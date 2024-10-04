e2b97a40a7ad0a34a5a37b86dab423c6acc7d372
Ezra <c14220315@john.petra.ac.id>
2024-10-04 18:18:19 +0700

    adding SafeArg

--- app/src/main/java/com/ezrabrilliant/mynavigation/CategoryFragment.kt
+++ app/src/main/java/com/ezrabrilliant/mynavigation/CategoryFragment.kt
@@ -5,19 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import com.ezrabrilliant.mynavigation.databinding.FragmentHomeBinding
+import com.ezrabrilliant.mynavigation.databinding.FragmentCategoryBinding
 
-class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+class CategoryFragment : Fragment() {
+
+    private var _binding: FragmentCategoryBinding? = null
     private val binding get() = _binding!!
+    companion object {
+        val EXTRA_NAME = "extra_name"
+        val EXTRA_STOCK = "extra_stock"
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
@@ -25,11 +29,11 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnCategory.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_categoryFragment)
-        )
-        binding.btnProfile.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_homeFragment_to_profileActivity)
+        binding.btnCategoryLifestyle.setOnClickListener { view ->
+            val mBundle = Bundle()
+            mBundle.putString(EXTRA_NAME, "Lifestyle")
+            mBundle.putLong(EXTRA_STOCK, 7)
+            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
         }
     }
 
